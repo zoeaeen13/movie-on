9950e8a4b534e8d87d9966359806f8bee8757336
Zoe Yu <zoeaeen13@gmail.com>
2021-09-25 23:29:39 +0800

    finish movie filter

--- src/pages/Browse/index.js
+++ src/pages/Browse/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react'
-import { find, debounce } from 'lodash'
-import { Dropdown, RangeSlider, Col } from 'rsuite'
+import { cloneDeep, isEmpty, find, debounce } from 'lodash'
+import { Dropdown, RangeSlider, Col, Cascader } from 'rsuite'
 import Layout from '../../components/App/Layout'
 import { MovieCard } from '../../components/Movie'
 import { MOVIE_TYPE } from '../../constants'
@@ -15,10 +15,10 @@ movieTypes.unshift({
   type: 'all',
 },)
 
-// 參考評分
+// 熱門參考
 const rankingType = [
   {
-    name: '參考',
+    name: '熱門參考',
     value: 'rating_total_amount',
   },
   {
@@ -35,6 +35,112 @@ const rankingType = [
   },
 ]
 
+// 選擇評分
+const score = [
+  {
+    value: 'imdb_rating',
+    label: 'IMDB',
+    children: [
+      {
+        value: 'imdb_rating=9',
+        label: '9 分以上',
+      },
+      {
+        value: 'imdb_rating=8',
+        label: '8 分以上',
+      },
+      {
+        value: 'imdb_rating=7',
+        label: '7 分以上',
+      },
+      {
+        value: 'imdb_rating=6',
+        label: '6 分以上',
+      },
+      {
+        value: 'imdb_rating=5',
+        label: '5 分以上',
+      },
+      {
+        value: 'imdb_rating=4',
+        label: '4 分以上',
+      },
+      {
+        value: 'imdb_rating=3',
+        label: '3 分以上',
+      },
+    ]
+  },
+  {
+    value: 'douban_rating',
+    label: '豆瓣',
+    children: [
+      {
+        value: 'douban_rating=9',
+        label: '9 分以上',
+      },
+      {
+        value: 'douban_rating=8',
+        label: '8 分以上',
+      },
+      {
+        value: 'douban_rating=7',
+        label: '7 分以上',
+      },
+      {
+        value: 'douban_rating=6',
+        label: '6 分以上',
+      },
+      {
+        value: 'douban_rating=5',
+        label: '5 分以上',
+      },
+      {
+        value: 'douban_rating=4',
+        label: '4 分以上',
+      },
+      {
+        value: 'douban_rating=3',
+        label: '3 分以上',
+      },
+    ]
+  },
+  {
+    value: 'tomato_rating',
+    label: '爛番茄',
+    children: [
+      {
+        value: 'tomato_rating=9',
+        label: '90 分以上',
+      },
+      {
+        value: 'tomato_rating=8',
+        label: '80 分以上',
+      },
+      {
+        value: 'tomato_rating=7',
+        label: '70 分以上',
+      },
+      {
+        value: 'tomato_rating=6',
+        label: '60 分以上',
+      },
+      {
+        value: 'tomato_rating=5',
+        label: '50 分以上',
+      },
+      {
+        value: 'tomato_rating=4',
+        label: '40 分以上',
+      },
+      {
+        value: 'tomato_rating=3',
+        label: '30 分以上',
+      },
+    ]
+  },
+]
+
 const startYear = 1980
 const endYear = 2021
 
@@ -43,7 +149,6 @@ const Browse = () => {
   const [loading, setLoading] = useState(false)
   const [movies, setMovies] = useState([])
   const bottomRef = useRef(null)
-  const [year, setYear] = useState([startYear, endYear])
   const [conditions, setConditions] = useState({
     sort: 'rating_total_amount',
     feature: '',
@@ -56,28 +161,20 @@ const Browse = () => {
   }
 
   const handleYearChange = ([start_year, end_year]) => {
-    setConditions({
-      ...conditions,
-      start_year,
-      end_year,
-    })
+    setConditions({...conditions, start_year, end_year })
   }
 
-  const debouncedChangeHandler = useCallback(
-    debounce(handleYearChange, 500)
-  , [])
+  const debouncedChangeHandler = debounce(handleYearChange, 500)
+
 
 
   const fetchMovies = async (start) => {
     setLoading(true)
     const params = {...conditions}
     params.start = start ? start : 0
+    console.log(params)
     const { data } = await getMovies(params)
-    if (start) {
-      setMovies(movies.concat(data))
-    } else {
-      setMovies(data)
-    }
+    setMovies(start ? movies.concat(data) : data)
     setLoading(false)
   }
 
@@ -109,7 +206,7 @@ const Browse = () => {
     <Layout className={`browse-wrapper ${loading && 'loading'}`}>
       <div className="conditions-wrapper">
         <Col md={2}>
-          <div style={{ height: 400, position: 'absolute' }}>
+          <div style={{ height: 400, position: 'fixed' }}>
             <RangeSlider
               defaultValue={[1980, 2021]}
               min={1980}
@@ -123,23 +220,53 @@ const Browse = () => {
             />
           </div>
         </Col>
-        <div className="flex-column">
-          <Dropdown
-            title={find(movieTypes, {id: conditions.feature}).name}
-            activeKey={conditions.feature}
-            onSelect={(value) => handleConditionChange('feature', find(movieTypes, {type: value}).id)}>
-            {movieTypes.map(({ id, name, type }, index) => {
-              return (<Dropdown.Item key={id+index} eventKey={type} children={name} />)
-            })}
-          </Dropdown>
-          <Dropdown
-            title={find(rankingType, {value: conditions.sort}).name}
-            activeKey={conditions.value}
-            onSelect={(value) => handleConditionChange('sort', value)}>
-            {rankingType.map(({ name, value }) => {
-              return (<Dropdown.Item key={value} eventKey={value} children={name} />)
-            })}
-          </Dropdown>
+        <div className="flex-space-between">
+          <div>
+            <Dropdown
+              title={find(movieTypes, {id: conditions.feature}).name}
+              activeKey={conditions.feature}
+              onSelect={(value) => handleConditionChange('feature', find(movieTypes, {type: value}).id)}>
+              {movieTypes.map(({ id, name, type }, index) => {
+                return (<Dropdown.Item key={id+index} eventKey={type} children={name} />)
+              })}
+            </Dropdown>
+            <Dropdown
+              title={find(rankingType, {value: conditions.sort}).name}
+              activeKey={conditions.value}
+              onSelect={(value) => handleConditionChange('sort', value)}>
+              {rankingType.map(({ name, value }) => {
+                return (<Dropdown.Item key={value} eventKey={value} children={name} />)
+              })}
+            </Dropdown>
+          </div>
+          <Cascader
+            data={score}
+            appearance="default"
+            placeholder="篩選評分"
+            style={{ width: 'min-content', backgroundColor: '#141414'}}
+            searchable={false}
+            onChange={(value) => {
+              if (isEmpty(value)) {
+                delete conditions.imdb_rating
+                delete conditions.douban_rating
+                delete conditions.tomato_rating
+                fetchMovies()
+              }
+            }}
+            onSelect={(item, selectedPaths) => {
+              if (selectedPaths.length < 2) return
+              const [parent, child] = selectedPaths
+              handleConditionChange(parent.value , child.value[child.value.length -1])  
+            }}
+            renderValue={() => {
+              let value = ''
+              const { imdb_rating, douban_rating, tomato_rating } = conditions
+              if (imdb_rating) value += `IMDB / ${imdb_rating} 分以上`
+              if (douban_rating) value += `${isEmpty(value) ? '' : '、'}豆瓣 / ${douban_rating} 分以上`
+              if (tomato_rating) value += `${isEmpty(value) ? '' : '、'}爛番茄 / ${tomato_rating} 分以上`
+              return value
+            }}
+          />
         </div>
       </div>
       <div className="movie-gallery">
